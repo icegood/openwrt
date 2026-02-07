4a69f494a7be4098139d049e1afca94969997c28
Serhii Ivanov <icegood1980@gmail.com>
2026-02-07 21:24:04 +0200

    Added ability to squashfs against different compressors and executables. So, we can easily choose best format to compress Anyway, issue is not so actual for 8Mb devices as many useful default features are fittable within 7Mb or so

--- include/image.mk
+++ include/image.mk
@@ -83,21 +83,51 @@ ifneq ($(CONFIG_JFFS2_LZMA),y)
   JFFS2OPTS += -x lzma
 endif
 
+define Image/mkfs/squashfs-params-check-pair
+  ifneq ($(CONFIG_TARGET_SQUASHFS_COMPRESSION_$(1)),)
+    ifeq ($(CONFIG_SQUASHFS_$(2)),y)
+      SQUASHFSCOMP := $(subst $\",,$(3) $(CONFIG_TARGET_SQUASHFS_COMPRESSION_PARAMS))
+    else
+      $$(error Cannot define squashfs compression CONFIG_TARGET_SQUASHFS_COMPRESSION_$(1) \
+        without kernel support for CONFIG_SQUASHFS_$(2))
+    endif
+  endif
+endef
+
+define Image/mkfs/squashfs-params-check
+  $(call Image/mkfs/squashfs-params-check-pair,GZIP,ZLIB,gzip)
+  $(call Image/mkfs/squashfs-params-check-pair,LZO,LZO,lzo)
+  $(call Image/mkfs/squashfs-params-check-pair,LZ4,LZ4,lz4)
+  $(call Image/mkfs/squashfs-params-check-pair,ZSTD,ZSTD,zstd)
+  $(call Image/mkfs/squashfs-params-check-pair,XZ,XZ,xz)
+endef
+
+define Image/mkfs/squashfs-get-exec-one
+  ifneq ($(CONFIG_TARGET_SQUASHFS_EXEC_$(1)),)
+    SQUASHFEXEC_SUFFIX := $(2)
+  endif
+endef
+
+define Image/mkfs/squashfs-get-exec
+  $(call Image/mkfs/squashfs-get-exec-one,squashfs_tools,)
+  $(call Image/mkfs/squashfs-get-exec-one,squashfskit4,4)
+  $(call Image/mkfs/squashfs-get-exec-one,squashfs_lzma,-lzma)
+endef
+
 JFFS2OPTS += $(MKFS_DEVTABLE_OPT)
 
-SQUASHFS_BLOCKSIZE := $(CONFIG_TARGET_SQUASHFS_BLOCK_SIZE)k
-SQUASHFSOPT := -b $(SQUASHFS_BLOCKSIZE)
-SQUASHFSOPT += -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1'
-SQUASHFSOPT += $(if $(CONFIG_SELINUX),-xattrs,-no-xattrs)
-SQUASHFSOPT += -block-readers $(CONFIG_TARGET_SQUASHFS_BLOCK_READERS)
-SQUASHFSOPT += -small-readers $(CONFIG_TARGET_SQUASHFS_SMALL_READERS)
-SQUASHFSCOMP := gzip
-LZMA_XZ_OPTIONS := -Xpreset 9 -Xe -Xlc 0 -Xlp 2 -Xpb 2
-ifeq ($(CONFIG_SQUASHFS_XZ),y)
-  ifneq ($(filter arm x86 powerpc sparc,$(LINUX_KARCH)),)
-    BCJ_FILTER:=-Xbcj $(LINUX_KARCH)
-  endif
-  SQUASHFSCOMP := xz $(LZMA_XZ_OPTIONS) $(BCJ_FILTER)
+ifeq ($(CONFIG_SQUASHFS),y)
+    SQUASHFS_BLOCKSIZE := $(CONFIG_TARGET_SQUASHFS_BLOCK_SIZE)k
+    SQUASHFSOPT := -b $(SQUASHFS_BLOCKSIZE)
+    SQUASHFSOPT += -p '/dev d 755 0 0' -p '/dev/console c 600 0 0 5 1'
+    SQUASHFSOPT += $(if $(CONFIG_SELINUX),-xattrs,-no-xattrs)
+    SQUASHFSOPT += -block-readers $(CONFIG_TARGET_SQUASHFS_BLOCK_READERS)
+    SQUASHFSOPT += -small-readers $(CONFIG_TARGET_SQUASHFS_SMALL_READERS)
+    $(eval $(call Image/mkfs/squashfs-params-check))
+    ifndef SQUASHFSCOMP
+        $(error None of squashfs compessors defined)
+    endif
+    $(eval $(call Image/mkfs/squashfs-get-exec))
 endif
 
 JFFS2_BLOCKSIZE ?= 64k 128k
@@ -280,7 +310,7 @@ $(eval $(foreach S,$(JFFS2_BLOCKSIZE),$(call Image/mkfs/jffs2/template,$(S))))
 $(eval $(foreach S,$(NAND_BLOCKSIZE),$(call Image/mkfs/jffs2-nand/template,$(S))))
 
 define Image/mkfs/squashfs-common
-	$(STAGING_DIR_HOST)/bin/mksquashfs4 $(call mkfs_target_dir,$(1)) $@ \
+	$(STAGING_DIR_HOST)/bin/mksquashfs$(SQUASHFEXEC_SUFFIX) $(call mkfs_target_dir,$(1)) $@ \
 		-nopad -noappend -root-owned \
 		-comp $(SQUASHFSCOMP) $(SQUASHFSOPT)
 endef
