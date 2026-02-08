c44b61dcc71460e1ad1adc601d72c4642d0364be
Serhii Ivanov <icegood1980@gmail.com>
2026-02-08 13:42:05 +0200

    Information about revision of basefiles. It is usefull for builds with possibility to add additionl packages via apk later. Two things to control to do so: 1) .vermagic to coincide with official release 2) version of basic files

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -12,6 +12,7 @@ ifeq ($(SDK),1)
   include $(TOPDIR)/include/version.mk
 else
   REVISION:=$(shell $(TOPDIR)/scripts/getver.sh)
+  $(info REVISION=$(REVISION))
   SOURCE_DATE_EPOCH:=$(shell $(TOPDIR)/scripts/get_source_date_epoch.sh)
 endif
 
@@ -20,7 +21,7 @@ export SOURCE_DATE_EPOCH
 export GIT_CONFIG_PARAMETERS='core.autocrlf=false'
 export GIT_ASKPASS:=/bin/true
 export MAKE_JOBSERVER=$(filter --jobserver%,$(MAKEFLAGS))
-export MAKE_PARALLEL_JOBS=$(filter -j%,$(MAKEFLAGS))
+export MAKE_PARALLEL_JOBS= $(filter -j%,$(MAKEFLAGS))
 export GNU_HOST_NAME:=$(shell $(TOPDIR)/scripts/config.guess)
 export HOST_OS:=$(shell uname)
 export HOST_ARCH:=$(shell uname -m)
